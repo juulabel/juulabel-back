83f2fa8449af5efcb1916d3d1719d41a402306fa
John <jinyngg@gmail.com>
2024-06-30 17:25:37 +0900

    init : init project

---
+++ src/test/java/com/juu/juulabel/JuulabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juu.juulabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuulabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
