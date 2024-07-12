568e6703e36ace8f23c578c2b274ef631d29fe3b
유현 <yh7119@gmail.com>
2024-07-12 14:35:20 +0900

    [#9] feat : 반환값 추가
    1. 회원가입 필요 여부 추가 반환

--- src/main/java/com/juu/juulabel/api/dto/response/LoginResponse.java
+++ src/main/java/com/juu/juulabel/api/dto/response/LoginResponse.java
@@ -3,6 +3,7 @@ package com.juu.juulabel.api.dto.response;
 import com.juu.juulabel.domain.dto.token.Token;
 
 public record LoginResponse(
-        Token token
+        Token token,
+        boolean isNewMember
 ) {
 }
