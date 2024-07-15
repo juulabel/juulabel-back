aeaa3f5fd10bcbabe8ef9d21aa85f4b93f4d0439
유현 <yh7119@gmail.com>
2024-07-16 02:27:14 +0900

    [#9] feat : 닉네임 중복 검사 기능 추가

--- src/main/java/com/juu/juulabel/domain/repository/reader/MemberReader.java
+++ src/main/java/com/juu/juulabel/domain/repository/reader/MemberReader.java
@@ -4,6 +4,7 @@ import com.juu.juulabel.common.exception.InvalidParamException;
 import com.juu.juulabel.common.exception.code.ErrorCode;
 import com.juu.juulabel.domain.annotation.Reader;
 import com.juu.juulabel.domain.entity.member.Member;
+import com.juu.juulabel.domain.enums.Provider;
 import com.juu.juulabel.domain.repository.jpa.MemberJpaRepository;
 import com.juu.juulabel.domain.repository.query.MemberQueryRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +16,20 @@ public class MemberReader {
     private final MemberJpaRepository memberJpaRepository;
     private final MemberQueryRepository memberQueryRepository;
 
-    public Member getByProviderId(String providerId) {
-        return memberQueryRepository.getByProviderId(providerId);
-    }
-
-    public boolean existsByProviderId(String providerId) {
-        return memberJpaRepository.existsByProviderId(providerId);
-    }
-
     public Member getByEmail(String email) {
         return memberJpaRepository.findByEmail(email)
                 .orElseThrow(() -> new InvalidParamException(ErrorCode.NOT_FOUND_MEMBER));
+    }
+
+    public boolean existsByEmailAndProvider(String email, Provider provider) {
+        return memberJpaRepository.existsByEmailAndProvider(email, provider);
+    }
+
+    public boolean existActiveEmail(String email) {
+        return memberQueryRepository.existActiveEmail(email);
+    }
+
+    public boolean existsByNickname(String nickname) {
+        return memberJpaRepository.existsByNickname(nickname);
     }
 }
