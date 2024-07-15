b10a3b29432bbaae477389a1828b35e64bd5b7ff
유현 <yh7119@gmail.com>
2024-07-15 13:51:38 +0900

    [#9] feat : 소셜 로그인 응답값 추가
    1. 응답 dto -> 해당 사용자의 고유 ID, 이메일 추가

--- src/main/java/com/juu/juulabel/api/dto/response/LoginResponse.java
+++ src/main/java/com/juu/juulabel/api/dto/response/LoginResponse.java
@@ -1,9 +1,11 @@
 package com.juu.juulabel.api.dto.response;
 
+import com.juu.juulabel.domain.dto.member.OAuthUser;
 import com.juu.juulabel.domain.dto.token.Token;
 
 public record LoginResponse(
         Token token,
-        boolean isNewMember
+        boolean isNewMember,
+        OAuthUser oAuthUser
 ) {
 }
