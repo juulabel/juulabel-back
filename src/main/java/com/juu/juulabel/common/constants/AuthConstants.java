9cc859b11f7bc36284bc4f417f6c00c70b4224dc
John <jinyngg@gmail.com>
2024-07-19 23:52:51 +0900

    [#22] chore : p6spy config 작성

--- src/main/java/com/juu/juulabel/common/constants/AuthConstants.java
+++ src/main/java/com/juu/juulabel/common/constants/AuthConstants.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public abstract class AuthConstants {
+public class AuthConstants {
 
     public static final String CODE = "code";
     public static final String REDIRECT_URI = "redirectUri";
