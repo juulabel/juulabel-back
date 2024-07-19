3b74cde2fbadefb40732beedb8da549c11c0bf07
John <jinyngg@gmail.com>
2024-07-19 10:55:15 +0900

    [#19] refactor : Jackson 필드 응답 리팩토링
    스네이크 케이스에서 카멜케이스로 변경

--- src/main/java/com/juu/juulabel/api/dto/request/OAuthLoginRequest.java
+++ src/main/java/com/juu/juulabel/api/dto/request/OAuthLoginRequest.java
@@ -1,6 +1,5 @@
 package com.juu.juulabel.api.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.juu.juulabel.common.constants.AuthConstants;
 import com.juu.juulabel.domain.dto.member.OAuthLoginInfo;
 import com.juu.juulabel.domain.enums.Provider;
@@ -12,7 +11,7 @@ import java.util.Map;
 public record OAuthLoginRequest(
     @NotBlank(message = "인가코드가 누락되었습니다.")
     String code,
-    @JsonProperty("redirectUri")
+    @NotNull(message = "리다이렉트 URI가 누락되었습니다.")
     String redirectUri,
     @NotNull(message = "가입 경로가 누락되었습니다.")
     Provider provider
--- src/main/java/com/juu/juulabel/domain/config/JacksonConfig.java
+++ src/main/java/com/juu/juulabel/domain/config/JacksonConfig.java
@@ -17,8 +17,8 @@ public class JacksonConfig {
 		objectMapper.registerModule(new JavaTimeModule());
 		// 날짜와 시간을 타임스탬프(숫자) 형식으로 쓰지 않도록 설정, ISO-8601 문자열 형식으로 날짜를 직렬화(2024-07-07T14:30:00)
 		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-		// JSON 필드 이름을 스네이크 케이스(snake_case)로 변환 설정(accessToken 필드는 access_token으로 변환)
-		objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+		// JSON 필드 이름을 카멜 케이스(lower_camel_case)로 변환 설정
+		objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE);
 		return objectMapper;
 	}
 }
