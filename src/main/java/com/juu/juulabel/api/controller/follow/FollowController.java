b8494913a328df767477049a821f85d5103c0841
John <jinyngg@gmail.com>
2024-07-25 10:31:45 +0900

    [#30] docs : 팔로우 스웨거 추가 작성

--- src/main/java/com/juu/juulabel/api/controller/follow/FollowController.java
+++ src/main/java/com/juu/juulabel/api/controller/follow/FollowController.java
@@ -12,11 +12,19 @@ import com.juu.juulabel.common.exception.code.SuccessCode;
 import com.juu.juulabel.common.response.CommonResponse;
 import com.juu.juulabel.domain.entity.member.Member;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "팔로우 API",
+        description = "팔로우, 언팔로우 등 팔로우 관련 API"
+)
 @RestController
 @RequestMapping(value = {"/v1/api"})
 @RequiredArgsConstructor
@@ -24,29 +32,48 @@ public class FollowController {
 
     private final FollowService followService;
 
-    @Operation(summary = "팔로우, 언팔로우")
+    @Operation(
+            summary = "팔로우, 언팔로우",
+            description = "회원을 팔로우 또는 언팔로우한다."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "팔로우, 언팔로우 성공"
+    )
     @PostMapping("/follow")
     public ResponseEntity<CommonResponse<FollowOrUnfollowResponse>> followOrUnfollow(
-            @LoginMember Member loginMember,
+            @Parameter(hidden = true) @LoginMember Member loginMember,
             @Valid @RequestBody FollowOrUnfollowRequest request) {
         return CommonResponse.success(SuccessCode.SUCCESS, followService.followOrUnfollow(loginMember, request));
     }
 
-    @Operation(summary = "팔로잉 목록 조회")
+    @Operation(
+            summary = "팔로잉 목록 조회",
+            description = "회원의 팔로잉 목록을 조회한다."
+    )
+    @Parameters(
+            @Parameter(name = "request", description = "팔로잉 리스트 조회 요청", required = true)
+    )
     @GetMapping("/members/{memberId}/followings")
     public ResponseEntity<CommonResponse<LoadFollowingListResponse>> loadFollowingList(
-            @LoginMember Member loginMember,
+            @Parameter(hidden = true) @LoginMember Member loginMember,
             @PathVariable Long memberId,
-            LoadFollowingListRequest request) {
+            @Valid LoadFollowingListRequest request) {
         return CommonResponse.success(SuccessCode.SUCCESS, followService.loadFollowingList(loginMember, memberId, request));
     }
 
-    @Operation(summary = "팔로워 목록 조회")
-    @PostMapping("/members/{memberId}/followers")
+    @Operation(
+            summary = "팔로워 목록 조회",
+            description = "회원의 팔로워 목록을 조회한다."
+    )
+    @Parameters(
+            @Parameter(name = "request", description = "팔로워 리스트 조회 요청", required = true)
+    )
+    @GetMapping("/members/{memberId}/followers")
     public ResponseEntity<CommonResponse<LoadFollowerListResponse>> loadFollowerList(
-            @LoginMember Member loginMember,
+            @Parameter(hidden = true) @LoginMember Member loginMember,
             @PathVariable Long memberId,
-            LoadFollowerListRequest request) {
+            @Valid LoadFollowerListRequest request) {
         return CommonResponse.success(SuccessCode.SUCCESS, followService.loadFollowerList(loginMember, memberId, request));
     }
 
--- src/main/java/com/juu/juulabel/api/dto/request/FollowOrUnfollowRequest.java
+++ src/main/java/com/juu/juulabel/api/dto/request/FollowOrUnfollowRequest.java
@@ -1,8 +1,11 @@
 package com.juu.juulabel.api.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "팔로우, 언팔로우 요청")
 public record FollowOrUnfollowRequest(
+        @Schema(description = "팔로우, 언팔로우 회원 고유 번호", example = "182", nullable = true)
         @NotNull(message = "followee 정보가 누락되었습니다.")
         Long followeeId
 ) {
