7f86d36aa65b37787573ac5f34a167abab9ec47a
John <jinyngg@gmail.com>
2024-07-25 10:30:41 +0900

    [#30] docs : 스웨거 설정 변경

--- src/main/java/com/juu/juulabel/api/config/SwaggerConfig.java
+++ src/main/java/com/juu/juulabel/api/config/SwaggerConfig.java
@@ -10,36 +10,42 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
-@OpenAPIDefinition(servers = {
-    @Server(url = "https://juulabel.shop", description = "Server"),
-    @Server(url = "http://localhost:8084", description = "Local")})
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://juulabel.shop", description = "Server"),
+                @Server(url = "http://localhost:8084", description = "Local")
+        }
+)
 @Configuration
 public class SwaggerConfig {
-    String JWT_SCHEME_NAME = "Authorization";
+
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-            .group("v1-definition")
-            .pathsToMatch("/**")
-            .build();
+                .group("v1")
+                .pathsToMatch("/**")
+                .build();
     }
+
     @Bean
     public OpenAPI springShopOpenAPI() {
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(JWT_SCHEME_NAME);
-
         Components components = new Components()
-            .addSecuritySchemes(JWT_SCHEME_NAME, new SecurityScheme()
-                .name(JWT_SCHEME_NAME)
-                .type(SecurityScheme.Type.APIKEY)
-                .in(SecurityScheme.In.HEADER)
-                .bearerFormat("JWT"));
+                .addSecuritySchemes(HttpHeaders.AUTHORIZATION, new SecurityScheme()
+                        .name(HttpHeaders.AUTHORIZATION)
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .bearerFormat("JWT"));
 
         return new OpenAPI()
-            .info(new Info().title("아파트너 프로젝트")
-                .description("파이널 프로젝트 api")
-                .version("v1"))
-            .addSecurityItem(securityRequirement)
-            .components(components);
+                .info(new Info()
+                        .title("주라벨")
+                        .description("주라벨 REST API")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(HttpHeaders.AUTHORIZATION))
+                .components(components);
     }
+
 }
