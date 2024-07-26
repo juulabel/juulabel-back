23a0c71e9993b4338786fd31fb01c9d1f571efa6
John <jinyngg@gmail.com>
2024-07-26 17:00:55 +0900

    [#26] feat : 시각 촉각 정보 추가

---
+++ src/main/java/com/juu/juulabel/domain/enums/alcohol/sensory/CarbonationLevel.java
@@ -0,0 +1,29 @@
+package com.juu.juulabel.domain.enums.alcohol.sensory;
+
+import com.juu.juulabel.domain.enums.Rateable;
+
+public enum CarbonationLevel implements Rateable {
+    NONE(1, "없음"),
+    LOW(2, "적음"),
+    MEDIUM(3, "중간"),
+    SOMEWHAT_HIGH(4, "있는 편"),
+    HIGH(5, "많음");
+
+    private final int score;
+    private final String description;
+
+    CarbonationLevel(int score, String description) {
+        this.score = score;
+        this.description = description;
+    }
+
+    @Override
+    public Integer getScore() {
+        return score;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/main/java/com/juu/juulabel/domain/enums/alcohol/sensory/ViscosityLevel.java
@@ -0,0 +1,30 @@
+package com.juu.juulabel.domain.enums.alcohol.sensory;
+
+import com.juu.juulabel.domain.enums.Rateable;
+
+public enum ViscosityLevel implements Rateable  {
+    NONE(1, "없음"),
+    LOW(2, "적음"),
+    MEDIUM(3, "중간"),
+    SOMEWHAT_HIGH(4, "있는 편"),
+    HIGH(5, "많음");
+
+    private final int score;
+    private final String description;
+
+    ViscosityLevel(int score, String description) {
+        this.score = score;
+        this.description = description;
+    }
+
+    @Override
+    public Integer getScore() {
+        return score;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+}
+
