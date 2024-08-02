d744550490632558c5727459dccdedec7dd1819b
유현 <yh7119@gmail.com>
2024-08-02 14:45:09 +0900

    [#32] refactor : 댓글 내용 최대 입력 글자 수 제한 변경

--- src/main/java/com/juu/juulabel/api/dto/request/WriteDailyLifeCommentRequest.java
+++ src/main/java/com/juu/juulabel/api/dto/request/WriteDailyLifeCommentRequest.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Size;
 public record WriteDailyLifeCommentRequest(
     @Schema(example = "이것은 내용입니다...")
     @NotNull(message = "내용이 누락되었습니다.")
-    @Size(max = 1200, message = "내용은 최대 1200글자까지 가능합니다.")
+    @Size(max = 600, message = "내용은 최대 600글자까지 가능합니다.")
     String content,
     @Schema(example = "1")
     @Positive
