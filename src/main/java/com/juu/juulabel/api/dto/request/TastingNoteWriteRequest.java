2bfbce6521e8bee7eb61d20725621c93308eb1af
John <jinyngg@gmail.com>
2024-08-06 18:30:45 +0900

    [#26] feat : flavor 팩토리 메소드 빌더 추가

--- src/main/java/com/juu/juulabel/api/dto/request/TastingNoteWriteRequest.java
+++ src/main/java/com/juu/juulabel/api/dto/request/TastingNoteWriteRequest.java
@@ -1,11 +1,13 @@
 package com.juu.juulabel.api.dto.request;
 
 import com.juu.juulabel.domain.dto.alcohol.AlcoholicDrinksDetails;
+import com.juu.juulabel.domain.enums.alcohol.flavor.FlavorType;
 import com.juu.juulabel.domain.enums.alcohol.sensory.SensoryType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
 import java.util.Map;
 
 @Schema(description = "시음노트 작성 요청")
@@ -17,10 +19,16 @@ public record TastingNoteWriteRequest(
         Long alcoholicDrinksId,
         @Schema(description = "색상 고유 번호", example = "7", nullable = true)
         Long colorId,
+        @Schema(description = "향 고유 번호 리스트", example = "[1, 4, 7]", nullable = true)
+        List<Long> scentIds,
         @Schema(description = "전통주 세부 정보")
         @Valid
         @NotNull(message = "전통주 세부 정보가 누락되었습니다.")
         AlcoholicDrinksDetails alcoholicDrinksDetails,
-        Map<SensoryType, String> sensoryMap
+        @Schema(description = "촉각 정보(sensory : name)")
+        Map<SensoryType, String> sensoryMap,
+        @Schema(description = "미각 정보(flavor : name)")
+        Map<FlavorType, String> flavorMap
+
 ) {
 }
--- src/main/java/com/juu/juulabel/api/factory/FlavorLevelFactory.java
+++ src/main/java/com/juu/juulabel/api/factory/FlavorLevelFactory.java
@@ -6,22 +6,23 @@ import com.juu.juulabel.common.exception.code.ErrorCode;
 import com.juu.juulabel.domain.dto.alcohol.FlavorLevel;
 import com.juu.juulabel.domain.dto.alcohol.FlavorTypeInfo;
 import com.juu.juulabel.domain.dto.alcohol.Level;
+import com.juu.juulabel.domain.embedded.Flavor;
 import com.juu.juulabel.domain.enums.Rateable;
 import com.juu.juulabel.domain.enums.alcohol.flavor.FlavorCommonLevel;
 import com.juu.juulabel.domain.enums.alcohol.flavor.FlavorType;
 import org.springframework.stereotype.Component;
 
-import java.util.EnumMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.lang.reflect.Method;
+import java.util.*;
 
 @Component
 public class FlavorLevelFactory {
     private final Map<FlavorType, Class<? extends Rateable>> flavorMap;
+    private final Map<FlavorType, Method> flavorMethodMap;
 
     public FlavorLevelFactory() {
         flavorMap = new EnumMap<>(FlavorType.class);
+        flavorMethodMap = new EnumMap<>(FlavorType.class);
         initialize();
     }
 
@@ -32,13 +33,41 @@ public class FlavorLevelFactory {
         flavorMap.put(FlavorType.UMAMI, FlavorCommonLevel.class);
         flavorMap.put(FlavorType.AFTERTASTE, FlavorCommonLevel.class);
         flavorMap.put(FlavorType.BODY, FlavorCommonLevel.class);
+
+        try {
+            flavorMethodMap.put(FlavorType.SWEETNESS, Flavor.FlavorBuilder.class.getMethod("sweetness", FlavorCommonLevel.class));
+            flavorMethodMap.put(FlavorType.SOURNESS, Flavor.FlavorBuilder.class.getMethod("sourness", FlavorCommonLevel.class));
+            flavorMethodMap.put(FlavorType.BITTERNESS, Flavor.FlavorBuilder.class.getMethod("bitterness", FlavorCommonLevel.class));
+            flavorMethodMap.put(FlavorType.UMAMI, Flavor.FlavorBuilder.class.getMethod("umami", FlavorCommonLevel.class));
+            flavorMethodMap.put(FlavorType.AFTERTASTE, Flavor.FlavorBuilder.class.getMethod("aftertaste", FlavorCommonLevel.class));
+            flavorMethodMap.put(FlavorType.BODY, Flavor.FlavorBuilder.class.getMethod("body", FlavorCommonLevel.class));
+        } catch (NoSuchMethodException e) {
+            throw new BaseException(e.getMessage(), ErrorCode.NO_SUCH_METHOD);
+        }
+    }
+
+    public Rateable getRateableByFlavorTypeAndLevel(FlavorType flavorType, String levelName) {
+        Class<? extends Rateable> enumClass = getEnumClass(flavorType);
+        return Arrays.stream(enumClass.getEnumConstants())
+                .filter(e -> e.getName().equals(levelName))
+                .findFirst()
+                .orElseThrow(() -> new InvalidParamException("Invalid flavor level : " + levelName, ErrorCode.INVALID_FLAVOR_LEVEL));
+    }
+
+    public void buildLevel(Flavor.FlavorBuilder flavorBuilder, FlavorType flavorType, Rateable level) {
+        Method method = flavorMethodMap.get(flavorType);
+        if (Objects.isNull(method)) {
+            throw new InvalidParamException("Unknown flavor type: " + flavorType);
+        }
+        try {
+            method.invoke(flavorBuilder, level);
+        } catch (Exception e) {
+            throw new InvalidParamException("Failed to build level for flavor type: " + flavorType);
+        }
     }
 
     public FlavorLevel getFlavorLevel(FlavorType flavorType) {
-        Class<? extends Rateable> enumClass = flavorMap.get(flavorType);
-        if (Objects.isNull(enumClass)) {
-            throw new InvalidParamException(ErrorCode.INVALID_FLAVOR_TYPE);
-        }
+        Class<? extends Rateable> enumClass = getEnumClass(flavorType);
 
         try {
             Rateable enumInstance = enumClass.getEnumConstants()[0];
@@ -55,4 +84,11 @@ public class FlavorLevelFactory {
                 .toList();
     }
 
+    private Class<? extends Rateable> getEnumClass(FlavorType flavorType) {
+        Class<? extends Rateable> enumClass = flavorMap.get(flavorType);
+        if (Objects.isNull(enumClass)) {
+            throw new InvalidParamException(ErrorCode.INVALID_FLAVOR_TYPE);
+        }
+        return enumClass;
+    }
 }
