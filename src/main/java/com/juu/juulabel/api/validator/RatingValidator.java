bb5dab4ec0e37d31d9960bbf84d8c0ddb0ad21fb
john <jinyngg@gmail.com>
2024-08-08 01:32:36 +0900

    [#26] feat : 달점 유효성 검사를 위한 커스텀 어노테이션 추가

---
+++ src/main/java/com/juu/juulabel/api/validator/RatingValidator.java
@@ -0,0 +1,30 @@
+package com.juu.juulabel.api.validator;
+
+import com.juu.juulabel.api.annotation.Rating;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.Objects;
+
+public class RatingValidator implements ConstraintValidator<Rating, Double> {
+
+    private double min;
+    private double max;
+
+    @Override
+    public void initialize(Rating annotation) {
+        this.min = annotation.min();
+        this.max = annotation.max();
+    }
+
+
+    @Override
+    public boolean isValid(Double value, ConstraintValidatorContext context) {
+        if (Objects.isNull(value)) {
+            return false;
+        }
+
+        return value >= min && value <= max && value % 0.25 == 0;
+    }
+
+}
