6c662e6230efde6eb0fa5aec7ac1082f16a479f2
유현 <yh7119@gmail.com>
2024-08-25 16:20:43 +0900

    [#70] feat : 검색 목록 조회 시 양조장 지역 추가 조회

--- src/main/java/com/juu/juulabel/domain/dto/alcohol/BrewerySummary.java
+++ src/main/java/com/juu/juulabel/domain/dto/alcohol/BrewerySummary.java
@@ -6,7 +6,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record BrewerySummary(
         @Schema(description = "양조장 고유 번호", example = "38")
         Long id,
-        @Schema(description = "양조장 이름", example = "느린마을 양조장")
-        String name
+        @Schema(description = "양조장 이름", example = "탁브루")
+        String name,
+        @Schema(description = "양조장 지역", example = "인천광역시 부평구")
+        String region
 ) {
 }
--- src/main/java/com/juu/juulabel/domain/repository/query/TastingNoteQueryRepository.java
+++ src/main/java/com/juu/juulabel/domain/repository/query/TastingNoteQueryRepository.java
@@ -46,7 +46,8 @@ public class TastingNoteQueryRepository {
                                 Projections.constructor(
                                         BrewerySummary.class,
                                         brewery.id,
-                                        brewery.name
+                                        brewery.name,
+                                        brewery.region
                                 )
                         )
                 )
