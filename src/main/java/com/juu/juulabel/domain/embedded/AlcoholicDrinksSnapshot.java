c760f81556c64cb582f1a1cfd3d1288467f3cb5d
유현 <yh7119@gmail.com>
2024-09-13 13:26:47 +0900

    [#107] refactor : 작성 처음 시점에 양조장 지역 추가 조회

--- src/main/java/com/juu/juulabel/domain/embedded/AlcoholicDrinksSnapshot.java
+++ src/main/java/com/juu/juulabel/domain/embedded/AlcoholicDrinksSnapshot.java
@@ -27,15 +27,18 @@ public class AlcoholicDrinksSnapshot {
     @Column(name = "brewery_name", columnDefinition = "varchar(100) comment '양조장 이름'")
     private String breweryName;
 
+    @Column(name = "brewery_region", columnDefinition = "varchar(100) comment '양조장 지역'")
+    private String breweryRegion;
+
     public static AlcoholicDrinksSnapshot of(AlcoholType alcoholType,
                                              AlcoholicDrinks alcoholicDrinks,
                                              Brewery brewery) {
         return AlcoholicDrinksSnapshot.builder()
-                .alcoholTypeName(alcoholType.getName())
-                .alcoholicDrinksName(alcoholicDrinks.getName())
-                .alcoholContent(alcoholicDrinks.getAlcoholContent())
-                .breweryName(brewery.getName())
-                .build();
+            .alcoholTypeName(alcoholType.getName())
+            .alcoholicDrinksName(alcoholicDrinks.getName())
+            .alcoholContent(alcoholicDrinks.getAlcoholContent())
+            .breweryName(brewery.getName())
+            .build();
     }
 
     public static AlcoholicDrinksSnapshot unofficial(String alcoholTypeName,
@@ -43,20 +46,21 @@ public class AlcoholicDrinksSnapshot {
                                                      Double alcoholContent,
                                                      String breweryName) {
         return AlcoholicDrinksSnapshot.builder()
-                .alcoholTypeName(alcoholTypeName)
-                .alcoholicDrinksName(alcoholicDrinksName)
-                .alcoholContent(alcoholContent)
-                .breweryName(breweryName)
-                .build();
+            .alcoholTypeName(alcoholTypeName)
+            .alcoholicDrinksName(alcoholicDrinksName)
+            .alcoholContent(alcoholContent)
+            .breweryName(breweryName)
+            .build();
     }
 
     public static AlcoholicDrinksSnapshot fromDto(AlcoholicDrinksDetails alcoholicDrinks) {
         return AlcoholicDrinksSnapshot.builder()
-                .alcoholTypeName(alcoholicDrinks.alcoholTypeName())
-                .alcoholicDrinksName(alcoholicDrinks.alcoholicDrinksName())
-                .alcoholContent(alcoholicDrinks.alcoholContent())
-                .breweryName(alcoholicDrinks.breweryName())
-                .build();
+            .alcoholTypeName(alcoholicDrinks.alcoholTypeName())
+            .alcoholicDrinksName(alcoholicDrinks.alcoholicDrinksName())
+            .alcoholContent(alcoholicDrinks.alcoholContent())
+            .breweryName(alcoholicDrinks.breweryName())
+            .breweryRegion(alcoholicDrinks.breweryRegion())
+            .build();
     }
 
 
