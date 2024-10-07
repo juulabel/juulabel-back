3684c04c359cb7e3c4e028f1fa60b6acfb58cb59
LEEKYUNGIMI <117709668+LEEKYUNGIMI@users.noreply.github.com>
2024-10-07 17:01:36 +0900

    [#116] feat : 전통주 상세소개 및 검색 정렬 기능 ( 상세소게 맛정보 level 추가 )

--- src/main/java/com/juu/juulabel/domain/dto/alcohol/FlavorDetail.java
+++ src/main/java/com/juu/juulabel/domain/dto/alcohol/FlavorDetail.java
@@ -2,10 +2,10 @@ package com.juu.juulabel.domain.dto.alcohol;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record SensoryDetail(
-        @Schema(description = "촉각 이름")
+public record FlavorDetail (
+        @Schema(description = "맛 이름")
         String name,
-        @Schema(description = "촉각 점수")
+        @Schema(description = "맛 점수")
         int score
-) {
+){
 }
