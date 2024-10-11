82cd68bfeaac5dfb0119ae1ab675456e80a0f6ce
유현 <yh7119@gmail.com>
2024-10-11 14:36:43 +0900

    [#156] feat : 스웨거 Tag값 추가 및 인증 로직 추가

--- src/main/java/com/juu/juulabel/api/controller/s3/S3Controller.java
+++ src/main/java/com/juu/juulabel/api/controller/s3/S3Controller.java
@@ -1,10 +1,14 @@
 package com.juu.juulabel.api.controller.s3;
 
+import com.juu.juulabel.api.annotation.LoginMember;
 import com.juu.juulabel.api.dto.response.ImageUploadResponse;
 import com.juu.juulabel.api.service.s3.S3Service;
 import com.juu.juulabel.common.exception.code.SuccessCode;
 import com.juu.juulabel.common.response.CommonResponse;
+import com.juu.juulabel.domain.entity.member.Member;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,10 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(
+    name = "이미지 파일 업로드 API",
+    description = "프론트 개발 편의를 위한 이미지 개별 업로드 API"
+)
 @RestController
 @RequestMapping(value = {"/v1/api/images"})
 @RequiredArgsConstructor
@@ -24,6 +32,7 @@ public class S3Controller {
     @Operation(summary = "이미지 파일 업로드")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CommonResponse<ImageUploadResponse>> uploadImage(
+        @Parameter(hidden = true) @LoginMember Member loginMember,
         @RequestPart(value = "image") MultipartFile image
     ) {
         return CommonResponse.success(SuccessCode.SUCCESS, s3Service.uploadMemberImage(image));
