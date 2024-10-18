dc47f851e20654f37105990214507bd00a9b05b3
유현 <yh7119@gmail.com>
2024-10-18 16:43:22 +0900

    [#160] feat : 알림 데이터 타입 수정 및 기능 테스트 완료

--- src/main/java/com/juu/juulabel/api/controller/notification/NotificationController.java
+++ src/main/java/com/juu/juulabel/api/controller/notification/NotificationController.java
@@ -2,15 +2,12 @@ package com.juu.juulabel.api.controller.notification;
 
 import com.juu.juulabel.api.annotation.LoginMember;
 import com.juu.juulabel.api.service.notification.NotificationService;
-import com.juu.juulabel.common.exception.code.SuccessCode;
-import com.juu.juulabel.common.response.CommonResponse;
 import com.juu.juulabel.domain.entity.member.Member;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,11 +30,11 @@ public class NotificationController {
         description = "알림을 위한 SSE 구독"
     )
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<CommonResponse<SseEmitter>> subscribe(
+    public SseEmitter subscribe(
         @Parameter(hidden = true) @LoginMember Member loginMember,
         @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId
     ) {
-        return CommonResponse.success(SuccessCode.SUCCESS, notificationService.subscribe(loginMember, lastEventId));
+        return notificationService.subscribe(loginMember, lastEventId);
     }
 
 }
