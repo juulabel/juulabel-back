9efb55ff7f599c35eb003c1c8227f2d1cc67af78
유현 <yh7119@gmail.com>
2024-10-18 15:25:32 +0900

    [#160] feat : 엔티티 칼럼 업데이트

--- src/main/java/com/juu/juulabel/domain/entity/notification/Notification.java
+++ src/main/java/com/juu/juulabel/domain/entity/notification/Notification.java
@@ -1,6 +1,6 @@
 package com.juu.juulabel.domain.entity.notification;
 
-import com.juu.juulabel.domain.base.BaseTimeEntity;
+import com.juu.juulabel.domain.base.BaseCreatedTimeEntity;
 import com.juu.juulabel.domain.entity.member.Member;
 import com.juu.juulabel.domain.enums.notification.NotificationType;
 import jakarta.persistence.*;
@@ -14,27 +14,27 @@ import lombok.*;
 @Table(
     name = "notification"
 )
-public class Notification extends BaseTimeEntity {
+public class Notification extends BaseCreatedTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", columnDefinition = "BIGINT UNSIGNED comment '알림 고유 번호'")
     private Long id;
 
-    private String title;
-
+    @Column(name = "content", nullable = false, columnDefinition = "varchar(100) comment '내용'")
     private String content;
 
-    private String url;
+    @Column(name = "related_url", nullable = false, columnDefinition = "varchar(100) comment '연관 URL'")
+    private String relatedUrl;
 
-    @Column(nullable = false, name = "is_read")
-    private Boolean isRead;
+    @Column(name = "is_read", nullable = false, columnDefinition = "tinyint comment '읽음 여부'")
+    private boolean isRead;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, name = "notification_type")
+    @Column(name = "notification_type", nullable = false, columnDefinition = "varchar(20) comment '알림 타입'")
     private NotificationType notificationType;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false, columnDefinition = "BIGINT UNSIGNED comment '회원 고유 번호'")
-    private Member member;
+    private Member receiver;
 }
