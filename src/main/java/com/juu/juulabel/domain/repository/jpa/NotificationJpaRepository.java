90ed68242fd995208813403cf01e9faeb2f4dd60
유현 <yh7119@gmail.com>
2024-10-20 23:25:25 +0900

    [#160] feat : 일상생활 댓글 알림 기능 추가

--- src/main/java/com/juu/juulabel/domain/repository/jpa/NotificationJpaRepository.java
+++ src/main/java/com/juu/juulabel/domain/repository/jpa/NotificationJpaRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationJpaRepository extends JpaRepository<Notification, Long> {
     void deleteByReceiverAndContentAndRelatedUrl(Member author, String content, String url);
+
+    void deleteByReceiverAndContentAndRelatedUrlAndCommentId(Member author, String content, String url, Long commentId);
 }
