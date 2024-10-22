d3118e3f4ddc2735c23842122e7c1b6bca6bd7e4
유현 <yh7119@gmail.com>
2024-10-22 15:44:00 +0900

    [#164] feat : 사용하지 않는 API 히든 처리

--- src/main/java/com/juu/juulabel/EbHealthCheck.java
+++ src/main/java/com/juu/juulabel/EbHealthCheck.java
@@ -1,8 +1,10 @@
 package com.juu.juulabel;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 public class EbHealthCheck {
     @GetMapping("/")
