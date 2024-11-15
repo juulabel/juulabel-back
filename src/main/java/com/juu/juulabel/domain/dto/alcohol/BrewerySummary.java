3d857aff7229e613c10263e72e55c2e94e827457
LEEKYUNGIMI <117709668+LEEKYUNGIMI@users.noreply.github.com>
2024-11-15 22:17:31 +0900

    [#175] refactor : 상세페이지 양조장 메세지 추가

--- src/main/java/com/juu/juulabel/domain/dto/alcohol/BrewerySummary.java
+++ src/main/java/com/juu/juulabel/domain/dto/alcohol/BrewerySummary.java
@@ -9,6 +9,15 @@ public record BrewerySummary(
         @Schema(description = "양조장 이름", example = "탁브루")
         String name,
         @Schema(description = "양조장 지역", example = "인천광역시 부평구")
-        String region
+        String region,
+        @Schema(description = "양조장 대표님 한마디", example = "아직 말씀 하시기 전 이에요!")
+        String message
 ) {
+        public BrewerySummary(Long id, String name, String region, String message) {
+                this.id = id;
+                this.name = name;
+                this.region = region;
+                // message가 null인 경우 기본 값으로 설정
+                this.message = (message != null && !message.isEmpty()) ? message : "아직 말씀 하시기 전 이에요!";
+        }
 }
