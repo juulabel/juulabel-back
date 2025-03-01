b1eb25aa5286f6a41820922302cd1c03f5f9db4c
유현 <yh7119@gmail.com>
2025-03-01 16:00:12 +0900

    [#212] fix : 이미지 없이 수정 시 null 처리되는 버그 해결

--- src/main/java/com/juu/juulabel/domain/entity/member/Member.java
+++ src/main/java/com/juu/juulabel/domain/entity/member/Member.java
@@ -99,6 +99,12 @@ public class Member extends BaseTimeEntity {
         this.gender = request.gender();
     }
 
+    public void updateProfileWithoutImage(UpdateProfileRequest request) {
+        this.nickname = request.nickname();
+        this.introduction = request.introduction();
+        this.gender = request.gender();
+    }
+
     public void deleteAccount() {
         this.deletedAt = LocalDateTime.now();
         this.profileImage = null;
