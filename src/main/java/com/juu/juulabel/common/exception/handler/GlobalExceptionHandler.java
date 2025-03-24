7aaac6233857f17c7362ccd5efd88f12a20ae5ee
권기오 <rldh9037@naver.com>
2025-03-24 13:04:22 +0900

    Revert [#219] Sentry 에러 모니터링 설정 변경

--- src/main/java/com/juu/juulabel/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/juu/juulabel/common/exception/handler/GlobalExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Objects;
 
@@ -56,11 +55,6 @@ public class GlobalExceptionHandler {
     public ResponseEntity<CommonResponse<String>> handle(MalformedJwtException e) {
         log.error("MalformedJwtException :", e);
         return CommonResponse.fail(ErrorCode.MALFORMED_JWT_EXCEPTION, "잘못된 토큰 형식입니다.");
-    }
-
-    @ExceptionHandler(NoResourceFoundException.class)
-    public void handle(NoResourceFoundException e) {
-        log.warn("NoResourceFoundException :", e);
     }
 
 }
