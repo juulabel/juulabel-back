001e13090b898cc1d69a8df8385f0c72c4a867ae
kwongio <rldh11111@naver.com>
2025-04-22 18:19:51 +0900

    refactor : enum description 추가

--- src/main/java/com/juu/juulabel/report/ReportStatus.java
+++ src/main/java/com/juu/juulabel/report/ReportStatus.java
@@ -1,5 +1,13 @@
 package com.juu.juulabel.report;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum ReportStatus {
-    PENDING, REVIEWING, REJECTED, APPROVED
+    PENDING("대기중"),
+    REVIEWING("검토중"),
+    REJECTED("거절"),
+    APPROVED("승인");
+
+    private final String description;
 }
--- src/main/java/com/juu/juulabel/report/ReportType.java
+++ src/main/java/com/juu/juulabel/report/ReportType.java
@@ -1,5 +1,13 @@
 package com.juu.juulabel.report;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum ReportType {
-    USER, TASTING_NOTE, DAILY_LIFE, COMMENT
+    USER("유저"),
+    TASTING_NOTE("시음노트"),
+    DAILY_LIFE("일상생활"),
+    COMMENT("댓글");
+
+    private final String description;
 }
